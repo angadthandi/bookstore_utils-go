09cc3d68b223656062e875cd59acaf16f2335285
Angad Thandi <angadthandi8@gmail.com>
2020-11-10 21:04:55 -0700

    Initial Version

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,46 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+func NewBadRequestError(msg string) *RestErr {
+	return &RestErr{
+		Message: msg,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(msg string) *RestErr {
+	return &RestErr{
+		Message: msg,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(msg string, err error) *RestErr {
+	ret := &RestErr{
+		Message: msg,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+	if err != nil {
+		ret.Causes = append(ret.Causes, err.Error())
+	}
+
+	return ret
+}
---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,22 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("error message", errors.New("database error"))
+
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "error message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+}
